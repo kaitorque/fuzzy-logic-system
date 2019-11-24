ade8b6a0b0f2b5bdf28340e8be78a3c50aa67f7a
Muhammad Ali <muhammad.aliakbr98@gmail.com>
2019-11-24 14:39:21 +0800

    Base Web Folder fuzzy_web

--- fuzzy_project/fuzzy_project/settings.py
+++ fuzzy_project/fuzzy_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fuzzy_web.apps.FuzzyWebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
